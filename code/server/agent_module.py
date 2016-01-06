392829a66c89b08f62ef61b0921cdf3384a38f59
Tom Wishaupt <github@tomwishaupt.nl>
2016-01-06 08:58:11 +0100

    Time-out verhoogd.

--- code/server/agent_module.py
+++ code/server/agent_module.py
@@ -80,7 +80,7 @@ class agent:
         # Deze functie heeft geen foutafhandeling. Dit moet in de code die de functie aan roept worden afgehandeld.
         binmsg = bytes(content, 'UTF-8') # Maak van een string een berg bytes met UTF-8 encoding.
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Maak een socket aan: IPv4 en TCP.
-        s.settimeout(6) # Geef op als er na 6 seconden geen antwoord is verkregen.
+        s.settimeout(15) # Geef op als er na 15 seconden geen antwoord is verkregen.
         s.connect((self.ip, 4568)) # Maak verbinding met het eerder opgegeven IP en TCP poort.
         s.send(struct.pack('!I', len(binmsg))) # Verstuur over de net opgezette verbinding: de lengte van het bericht *als een (4 byte lang) unsigned integer (big-endian)*.
         s.send(binmsg) # Nu de ontvanger weet hoe lang het volgende bericht is, kan het bericht worden verstuurd.
